027c7429029263143e2adb05af0fe27dbcfdfd96
Saurabh Kumar <43825814+saurabhkumar23@users.noreply.github.com>
2021-05-24 10:38:42 +0530

    sheet add button handle & switching between sheets handle

---
+++ script.js
@@ -0,0 +1,35 @@
+let addSheetBtn = document.querySelector('.add-sheet_btn-container')
+let sheetList   = document.querySelector('.sheet-list')
+let firstSheet = document.querySelector('.sheet')
+
+// initial manual work, for first sheet only
+firstSheet.addEventListener('click',handleSheet)
+
+// on click - append new sheet at the end (add class,update idx,set as .active)
+addSheetBtn.addEventListener('click',function(){
+    let allSheets = document.querySelectorAll('.sheet')
+    let lastSheet = allSheets[allSheets.length-1]               // using last sheet to update idx
+    let lastIdx = lastSheet.getAttribute('idx')
+    lastIdx = Number(lastIdx)
+    let newSheet = document.createElement('div')           // creating and append new sheet
+    for(let i=0;i<allSheets.length;i++){                      
+        allSheets[i].classList.remove('active')          // last sheet become .active by deactivating all others
+    }
+    newSheet.setAttribute('class','sheet active')
+    newSheet.setAttribute('idx',`${lastIdx+1}`)
+    newSheet.innerText = `Sheet ${lastIdx+2}`
+    sheetList.appendChild(newSheet)
+
+    // on click - that sheet become .active by deactivating all others (do it for first sheet manually)
+    newSheet.addEventListener('click',handleSheet)
+})
+
+// that sheet become .active by deactivating all others
+function handleSheet(e){
+    let currentSheet = e.currentTarget
+    let allSheets = document.querySelectorAll('.sheet')         // again get, bcz of last sheet updation
+    for(let i=0;i<allSheets.length;i++){                      
+        allSheets[i].classList.remove('active')
+    }
+    currentSheet.classList.add('active')
+}
